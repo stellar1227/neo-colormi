4b3fdbc2aaf32ee61c1d2dc053812b9599b99d9c
이미진 <stella@imijin-ui-MacBookPro-2.local>
2025-04-21 21:51:11 +0900

    fix: productImageSlider작업

--- src/js/colormi-ui.js
+++ src/js/colormi-ui.js
@@ -1,4 +1,39 @@
 document.addEventListener("DOMContentLoaded", () => {
-    console.log("페이지가 로드되었습니다!");
-  });
+  const thumbNailGallery = document.getElementById("thumbNailGallery");
+  const imgItemWrap = thumbNailGallery.querySelector(".img-item-wrap");
+  const imgItems = imgItemWrap.querySelectorAll(".img-item");
+  const btnPrev = thumbNailGallery.querySelector(".btn-prev");
+  const btnNext = thumbNailGallery.querySelector(".btn-next");
+  const imgDetail = thumbNailGallery.querySelector(".product-img-detail img");
+  let currentIndex = 0;
+  const imgItemLength = imgItems.length;
+  const MARGIN_TOP = 12;
+  const itemListNum = 4;
+  const imgItemHeight = imgItems[0].offsetHeight + MARGIN_TOP;
   
+  btnPrev.addEventListener("click", () => {
+    if (currentIndex > 0) {
+      currentIndex--;
+      imgItemWrap.style.transform = `translateY(-${currentIndex === 0 ? MARGIN_TOP : (currentIndex * imgItemHeight) + MARGIN_TOP }px)`;
+      return;
+    }
+    alert('첫번째 이미지입니다.')
+  });
+
+  btnNext.addEventListener("click", () => {
+    if (currentIndex < imgItemLength - itemListNum) {
+      currentIndex++;
+      imgItemWrap.style.transform = `translateY(-${currentIndex === imgItemLength - itemListNum ? MARGIN_TOP : (currentIndex * imgItemHeight) + MARGIN_TOP }px)`;
+      return;
+    }
+    alert('마지막 이미지입니다.')
+  });
+
+
+  // imgItems에 마우스엔터 시 product-img-detail에 해당 이미지 보여주기
+  imgItems.forEach((item) => {
+    item.addEventListener("mouseenter", () => {
+      imgDetail.src = item.querySelector("img").src;
+    });
+  });
+});
